be32c492f23609c7fc2ace12da891cb8cd226e9b
Maboulmagd <Mohamedrefat007@gmail.com>
2021-12-17 00:42:51 -0600

    Initial default environment.
    This is a clean slate.

---
+++ Test/_UnitTestConfiguration.h
@@ -0,0 +1,32 @@
+//-----------------------------------------------------------------------------
+// Copyright Ed Keenan 2019
+// Optimized C++
+//----------------------------------------------------------------------------- 
+
+#ifndef UNIT_TEST_CONFIGURATION_H
+#define UNIT_TEST_CONFIGURATION_H
+
+// Make sure all are enabled for final submission...
+//      I grade what ever is set in this file - Keenan
+
+#define Mem1_Initialize_Test_Enable                        true
+#define Mem2_Allocate_Everything_Test_Enable               false
+#define Mem3_Allocate_Everything_Free_it_Test_Enable       false
+#define Mem4_Partial_allocate_Test_Enable                  false
+#define Mem5_Partial_allocate_free_Test_Enable             false
+#define Mem6_Allocate_3_bottom_up_free_Test_Enable         false
+#define Mem7_Allocate_3_Top_down_free_Test_Enable          false
+#define Mem8_Allocate_3_middle_up_free_Test_Enable         false
+#define Mem9_Allocate_3_middle_down_free_Test_Enable       false
+#define Mem10_Allocate_3_100_middle_up_free_Test_Enable    false
+#define Mem11_Allocate_3_100_middle_down_free_Test_Enable  false
+#define Mem12_Allocate_3_100_bottom_up_free_Test_Enable    false
+#define Mem13_Allocate_3_100_Top_Down_free_Test_Enable     false
+#define Mem14_combo_1_Test_Enable                          false
+#define Mem15_combo_2_Test_Enable                          false
+#define Mem16_Secret_pointers_Test_Enable                  false
+#define Mem17_Stress_timing_Test_Enable                    false
+
+#endif
+
+// ---  End of File ---------------
